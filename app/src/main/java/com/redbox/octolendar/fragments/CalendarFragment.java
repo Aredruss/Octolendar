f8ecc6debe2bbc0065e142482724671a533d47c0
Aredruss <a.e.medyanik@yandex.ru>
2019-05-01 00:27:56 +0300

    Basic statistics implementation.

--- app/src/main/java/com/redbox/octolendar/fragments/CalendarFragment.java
+++ app/src/main/java/com/redbox/octolendar/fragments/CalendarFragment.java
@@ -16,31 +16,38 @@ import android.widget.TextView;
 
 import com.redbox.octolendar.R;
 import com.redbox.octolendar.parser.GetJson;
+import com.redbox.octolendar.singleton.App;
 import com.redbox.octolendar.utilities.DateTimeUtilityClass;
 
 public class CalendarFragment extends Fragment {
 
-    private String percentString;
+    private String percentString= "%d%% of the month has passed";
     private CalendarView calendarView;
     private ProgressBar progressBar;
     private TextView progressTextView;
+    private TextView countTextView;
     private String date;
     private TextView requestTextView;
     private Button requestButton;
     private String jsonResponse;
+    private App.EventDatabase database;
+    private App.EventDao dao;
+    private int count;
+    private int completed;
 
     private OnCalendarInteractionListener interactionListener;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
         View fragmentView = inflater.inflate(R.layout.calendar_fragment, container, false);
 
+        countTextView = fragmentView.findViewById(R.id.countTextView);
         calendarView = fragmentView.findViewById(R.id.fragmentCalendarView);
         progressTextView = fragmentView.findViewById(R.id.monthProgressTextView);
         progressBar = fragmentView.findViewById(R.id.monthProgressBar);
-        percentString = "%d%% of the month has passed";
+
         date = DateTimeUtilityClass.getToday();
+        setStatistics();
 
         progressTextView.setText(String.format(percentString, DateTimeUtilityClass.getMonthProgress()));
         progressBar.setProgress(DateTimeUtilityClass.getMonthProgress());
@@ -98,6 +105,19 @@ public class CalendarFragment extends Fragment {
 
     public void sendDate() {
         interactionListener.onCalendarInteraction(date);
+    }
+
+    public void setStatistics(){
+        String countString = "You have %d planned events";
+        App.EventDatabase database = App.getInstance().getEventDatabase();
+        App.EventDao dao = database.eventDao();
+
+        count = dao.getAll().size();
+        if(count ==1 ) {
+          countString = countString.substring(0,countString.length()-1);
+        }
+
+        countTextView.setText(String.format(countString, count));
     }
 
 }
